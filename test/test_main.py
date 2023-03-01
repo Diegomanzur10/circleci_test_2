f1ba294927432fa3c934e2b69b6c3532bb02a25d
dmanzur <dmanzur@videoamp.com>
2023-03-01 10:23:30 -0500

    Primer commit

---
+++ test/test_main.py
@@ -0,0 +1,42 @@
+from code.main import suma, resta, multiplicacion, division
+import pytest
+
+@pytest.mark.parametrize(
+    (       ## Set de variables de la funcion
+        "a",
+        "b",
+        "valor_esperado",
+    ),
+    [
+        (1,
+         1,
+         2,),
+
+        (1,
+         2,
+         3,),
+
+        (1,
+         3,
+         4,),
+
+        (1,
+         4,
+         5,),
+    ],
+
+    ids=[
+        "suma_1_1",
+        "suma_1_2",
+        "suma_1_3",
+        "suma_1_4",
+    ]
+)
+def test_suma(a, b, valor_esperado):
+    assert suma(a,b) == valor_esperado
+
+@pytest.mark.xfail
+def test_division():
+    assert division(1, 1) == 1
+
+
